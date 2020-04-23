30e91613732bb788b3176b6f6ef5c48ae015140b
Manoel <manoel.lobo@gmail.com>
2020-04-23 09:50:09 -0300

    feat: delete a repository

--- src/App.js
+++ src/App.js
@@ -22,7 +22,9 @@ function App() {
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`);
+
+    setRepositories(repositories.filter((repository) => repository.id !== id));
   }
 
   return (
